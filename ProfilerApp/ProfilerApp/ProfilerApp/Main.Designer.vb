44da5040bacc507169e93a9bd1291b7a3258c78b
Pharaia <119729318+Pharaia@users.noreply.github.com>
2023-12-02 16:05:05 +0800

    Add files via upload

---
+++ ProfilerApp/ProfilerApp/ProfilerApp/Main.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.id = New System.Windows.Forms.Label()
+        Me.pass = New System.Windows.Forms.TextBox()
+        Me.uname = New System.Windows.Forms.TextBox()
+        Me.email = New System.Windows.Forms.TextBox()
+        Me.fname = New System.Windows.Forms.TextBox()
+        Me.lname = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(192, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 25)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Update"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(149, 31)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 16)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "WELCOME:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.id)
+        Me.GroupBox1.Controls.Add(Me.pass)
+        Me.GroupBox1.Controls.Add(Me.uname)
+        Me.GroupBox1.Controls.Add(Me.email)
+        Me.GroupBox1.Controls.Add(Me.fname)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 158)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(312, 224)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Personal Data"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(-3, 177)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "PASSWORD:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(0, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "USERNAME:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "EMAIL:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "FULLNAME:"
+        '
+        'id
+        '
+        Me.id.AutoSize = True
+        Me.id.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.id.Location = New System.Drawing.Point(6, 27)
+        Me.id.Name = "id"
+        Me.id.Size = New System.Drawing.Size(112, 20)
+        Me.id.TabIndex = 4
+        Me.id.Text = "USER ID: ID"
+        '
+        'pass
+        '
+        Me.pass.Location = New System.Drawing.Point(89, 174)
+        Me.pass.Name = "pass"
+        Me.pass.Size = New System.Drawing.Size(211, 20)
+        Me.pass.TabIndex = 3
+        '
+        'uname
+        '
+        Me.uname.Location = New System.Drawing.Point(89, 138)
+        Me.uname.Name = "uname"
+        Me.uname.Size = New System.Drawing.Size(211, 20)
+        Me.uname.TabIndex = 2
+        '
+        'email
+        '
+        Me.email.Location = New System.Drawing.Point(89, 101)
+        Me.email.Name = "email"
+        Me.email.Size = New System.Drawing.Size(211, 20)
+        Me.email.TabIndex = 1
+        '
+        'fname
+        '
+        Me.fname.Location = New System.Drawing.Point(89, 61)
+        Me.fname.Name = "fname"
+        Me.fname.Size = New System.Drawing.Size(211, 20)
+        Me.fname.TabIndex = 0
+        '
+        'lname
+        '
+        Me.lname.AutoSize = True
+        Me.lname.Location = New System.Drawing.Point(265, 34)
+        Me.lname.Name = "lname"
+        Me.lname.Size = New System.Drawing.Size(39, 13)
+        Me.lname.TabIndex = 8
+        Me.lname.Text = "Label1"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(374, 474)
+        Me.Controls.Add(Me.lname)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Main"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents id As Label
+    Friend WithEvents pass As TextBox
+    Friend WithEvents uname As TextBox
+    Friend WithEvents email As TextBox
+    Friend WithEvents fname As TextBox
+    Friend WithEvents lname As Label
+End Class
---
+++ ProfilerApp/ProfilerApp/ProfilerApp/Main.vb
@@ -0,0 +1,52 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Main
+    Public Sub searchrec(ByRef SQLSTATEMENT As String)
+
+        SQLCONN.ConnectionString = con
+
+        If SQLCONN.State = ConnectionState.Closed Then
+
+            SQLCONN.Open()
+
+            Dim dt As New DataTable
+
+            Dim MyCommand As New MySqlCommand(SQLSTATEMENT, SQLCONN)
+
+            Dim myDataAdapter As New MySqlDataAdapter(SQLSTATEMENT, SQLCONN)
+            myDataAdapter.Fill(dt)
+
+            Dim sqlrdr As MySqlDataReader
+
+            sqlrdr = MyCommand.ExecuteReader(System.Data.CommandBehavior.CloseConnection)
+
+            While sqlrdr.Read()
+
+
+
+                id.Text = sqlrdr("id")
+
+                fname.Text = sqlrdr("Fullname")
+
+                fname.Text = sqlrdr("Fullname")
+
+                email.Text = sqlrdr("Email")
+
+                uname.Text = sqlrdr("Username")
+
+                pass.Text = sqlrdr("Password")
+
+            End While
+
+        End If
+
+        SQLCONN.Close()
+
+        SQLCONN.Dispose()
+
+    End Sub
+
+    Private Sub Updbtn_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim SQLSTATEMENT As String = "UPDATE `user` SET `Fullname`='" & fname.Text & "',`Email`='" & email.Text & "',`Username`='" & uname.Text & "',`Password`='" & pass.Text & "' Where id = '" & id.Text & "'"
+    End Sub
+End Class
